7f5cbc8447b5455ad5d7a48f525c1432ebe5c597
Aayush <aayushratnasthapit@gmail.com>
2019-04-22 23:00:28 +0545

    Integrated the React App to the project.

---
+++ frontend/src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Route, BrowserRouter as Router} from 'react-router-dom';
+
+// Authentication
+import SignIn from './components/Authentication/SignIn'
+
+const Routes=()=>{
+    return(
+<Router>
+    <Route path='/signin' component={SignIn}/>
+</Router>
+    )
+}
+
+export default Routes;
